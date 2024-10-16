7980ddff14249b32f79d14476eb8d58609731b0c
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2024-10-16 19:07:36 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,29 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Uniperf kernel by RifsxD @ https://github.com/rifsxd
+kernel.string=Uniperf kernel by rifsxd @ github
 do.devicecheck=0
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=RMX3511
-supported.versions=12 - 14
+supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
 # boot shell variables
 BLOCK=auto;
-IS_SLOT_DEVICE=1;
+IS_SLOT_DEVICE=auto;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
